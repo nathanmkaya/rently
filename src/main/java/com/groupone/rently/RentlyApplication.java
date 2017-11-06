c999119549690d17dc3c66254dd84f85bc27a85f
Nathan Mkaya <nathanmkaya@gmail.com>
2017-11-06 22:40:14 +0300

    project setup

---
+++ src/main/java/com/groupone/rently/RentlyApplication.java
@@ -0,0 +1,12 @@
+package com.groupone.rently;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentlyApplication.class, args);
+	}
+}
